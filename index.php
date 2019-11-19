ee29ff00806c3fc718d51e9850a4cd987f397394
dkchaudhary7 <devashishkumarchoudhary@gmail.com>
2019-11-19 13:27:18 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>singin and signup</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<br>
+<br>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			 <div class="panel panel-danger">
+				  <div class="panel-heading">Onlineexam</div>
+				  <div class="panel-body">onlineexam </div>
+			</div>
+		</div>
+    </div>
+</div>
+
+
+
+
+	<div class="container">
+		<div class="row">
+			     <div class="col-sm-4">
+					  <div class="panel panel-info">
+                      <div class="panel-heading">Signin Form</div>
+						<div class="panel-body">
+						<?php
+						if(isset($_GET['run'])  && $_GET['run']=="failed")
+						{
+							echo "Your email or password is not correct";
+						}
+						
+						
+						?>
+							  <form role="form" action="signin_sub.php" method="post">
+								<div class="form-group">
+								  <label for="email">Email:</label>
+								  <input type="email" class="form-control" name="e" id="email" placeholder="Enter email">
+								</div>
+								<div class="form-group">
+								  <label for="pwd">Password:</label>
+								  <input type="password" class="form-control" name="p" id="pwd" placeholder="Enter password">
+								</div>
+								<button type="submit" class="btn btn-default">Submit</button>
+							  </form>
+						  </div>
+						  </div>
+					  </div>
+
+	<div class="container">
+		<div class="col">
+			     <div class="col-sm-2">
+					  <div class="panel panel-info">
+                      <div class="panel-heading">adminlogin</div>
+						<div class="panel-body">
+						<?php
+						if(isset($_GET['run'])  && $_GET['run']=="failed")
+						{
+							echo "Your email or password is not correct";
+						}
+						
+						
+						?>
+							  <form role="form" action=".php" method="post">
+								<div class="form-group">
+								  <label for="email">Email:</label>
+								  <input type="email" class="form-control" name="e" id="email" placeholder="Enter email">
+								</div>
+								<div class="form-group">
+								  <label for="pwd">Password:</label>
+								  <input type="password" class="form-control" name="p" id="pwd" placeholder="Enter password">
+								</div>
+								<button type="submit" class="btn btn-default">Submit</button>
+							  </form>
+						  </div>
+						  </div>
+					  </div>
+				 
+				   <div class="col-sm-8">
+							<div class="panel panel-info">
+								
+								<div class="panel-heading">Signup Form</div>
+							<div class="panel-body">
+							<?php 
+							if(isset($_GET['run'])&& $_GET['run']=="success")
+							{
+								echo "<mark>Your registration successfully done</mark>";
+							} 
+							?>
+							  <form role="form" method="post" action="signup_sub.php" enctype="multipart/form-data"  >
+							  	<div class="form-group">
+								  <label for="name">Name:</label>
+								  <input type="text" class="form-control" name="n" id="name" placeholder="Enter name">
+								</div>
+								<div class="form-group">
+								  <label for="email">Email:</label>;
+								  <input type="email" class="form-control" name="e" id="email" placeholder="Enter email">
+								</div>
+								<div class="form-group">
+								  <label for="pwd">Password:</label>
+								  <input type="password" class="form-control" name="p" id="pwd" placeholder="Enter password">
+								
+							  </form>
+						  </div>
+						  </div>
+			      </div>
+	   </div>
+   </div>
+   
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
